43f699e31bd8ecf7514a88628e6d067e7bfaef3b
JuanRodriguezAbarca <juarabarca@gmail.com>
2015-07-01 16:45:51 +0200

    Commit From Project

--- src/main/java/utils/PageModel.java
+++ src/main/java/utils/PageModel.java
@@ -3,6 +3,7 @@ package utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -13,29 +14,24 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElem
 /**
  * Created by Juan_Rodriguez on 6/30/2015.
  */
-public abstract class PageModel  {
+public abstract class PageModel {
 
-    WebDriverClass webDriverClass = new WebDriverClass("firefox");
+    WebDriver driver = WebDriverClass.getInstance();
 
-    private static final int DRIVER_WAIT_TIME = 60;
-    protected static final Logger LOG = LoggerFactory.getLogger(PageModel.class);
 
-    public WebDriver getDriver() {
-        return webDriverClass.getRemoteWebDriver();
+    public WebElement waitForExpectedElement(By by) {
+
+        return (new WebDriverWait(driver, 60)).until(ExpectedConditions.presenceOfElementLocated(by));
     }
-
-
 
     public void navigate(String url) {
-        LOG.info("Navigating to : " + url);
-        getDriver().get(url);
+        driver.get(url);
     }
 
-    public WebElement waitForExpectedElement(final By by) {
-//        Wait<WebDriver> wait = new WebDriverWait(getDriver(), DRIVER_WAIT_TIME);
-//        return wait.until(visibilityOfElementLocated(by));
-        return getDriver().findElement(by);
+    public void killBrowser() {
+        driver.close();
     }
-
-
 }
+
+
+
